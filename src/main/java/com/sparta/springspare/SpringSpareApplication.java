05dbecf98f103c0865cffb7b2a95c46e6ff52165
eunkyo <jyk8929@naver.com>
2025-03-02 15:31:24 +0900

    init commit

---
+++ src/main/java/com/sparta/springspare/SpringSpareApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springspare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSpareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springspare/SpringSpareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springspare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
